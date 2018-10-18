ce013522cc44dfa8496fdfd18285bd2a1820fa2a
electron101 <sabashlive@gmial.com>
2018-10-18 20:52:31 +0300

    Третий урок, первая задача 100%

--- Lesson3/PermMissingElem/perm_missing_elem.c
+++ Lesson3/PermMissingElem/perm_missing_elem.c
@@ -45,24 +45,62 @@ Write an efficient algorithm for the following assumptions:
 /*     return -1; */
 /* } */
 
+//100%
+/* int solution(int N) { */
+/* 	// write your code in C99 (gcc 6.2.0) */
+/*  */
+/* 	int i; */
+/* 	int arch_size = sizeof(int)*8;; */
+/* 	int binary_arr[arch_size]; */
+/* 	int one       = 0; */
+/* 	int tmp_count = 0; */
+/* 	int count     = 0; */
+/*  */
+/* 	for (i = arch_size - 1; i >= 0; --i) */
+/* 		binary_arr[i]=(N >> i) % 2; */
+/*  */
+/* 	for (i = 0; i < arch_size; ++i) { */
+/* 		if (binary_arr[i] == 1) { */
+/* 			if (tmp_count > 0) { */
+/* 				if(tmp_count > count)  */
+/* 					count = tmp_count; */
+/* 				tmp_count = 0; */
+/* 			} */
+/* 			one = 1; */
+/* 			continue; */
+/* 		} */
+/*     	if (binary_arr[i] == 0 && one == 1) */
+/* 			tmp_count++; */
+/* 	} */
+/* 	return count; */
+/* } */
 
+//100%
 int solution(int A[], int N) {
-    // write your code in C99 (gcc 6.2.0)
-    int i;
-    /* int B[N + 1]; */
-    int B[100000] = {0};
+        // write your code in C99 (gcc 6.2.0)
+        int i;
+        int tmp_count = 0;
+        int tmp_zero  = -1;
+        int B[1000000] = {0};
 
-    for (i = 0; i < N; ++i) 
-	    B[A[i]] = 1;
-   
-    for (i = 1; i < N + 1; ++i) {
-	    if (B[i] != 0)
-		    return i;
-    }
-    return -1;
-}
+        for (i = 0; i < N; ++i) 
+                B[A[i]] = 1;
 
-
+        for (i = 1; i < 1000000; ++i) {
+                if (tmp_count == N) {
+                    if (tmp_zero == -1)
+                        tmp_zero = N + 1;
+                    return tmp_zero;
+                }
+       
+       		if (B[i] == 1)
+                        tmp_count++;
+                
+		if (B[i] == 0)
+                        tmp_zero = i;
+        }
+        return tmp_zero;
+} 
 
 int main ()
 {
