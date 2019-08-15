701a05bc823bd64cb8a00f4c6d1b709f66239f3b
electron101 <sabashlive@gmail.com>
2019-08-15 10:05:12 +0300

    odd_occurrences_in_array решил на 100 процентов

--- Lesson2/OddOccurrencesInArray/odd_occurrences_in_array.c
+++ Lesson2/OddOccurrencesInArray/odd_occurrences_in_array.c
@@ -77,19 +77,16 @@ Write an efficient algorithm for the following assumptions:
 /* 	return -1; */
 /* } */
 
+/* 100% */
 int solution(int A[], int N) {
 	// write your code in C99 (gcc 6.2.0)
 	int i;
-	int B[1000000] = {0};
+	int result = A[0];
 
-	for (i = 0; i < N; ++i) 
-		B[A[i]]++;
-
-	for (i = 0; i < 1000000; ++i) {
-		if (B[i] == 1)
-			return i;
+	for (i = 1; i < N; ++i) {
+		result ^= A[i];
 	}
-	return -1;
+	return result;
 }
 
 int main ()
