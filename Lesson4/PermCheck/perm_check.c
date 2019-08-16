5b65e665adb6dcfe8b42a1b738a07eb14e97c4fc
electron101 <sabashlive@gmail.com>
2019-08-16 15:17:46 +0300

    perm_check выполнил на 100%

--- Lesson4/PermCheck/perm_check.c
+++ Lesson4/PermCheck/perm_check.c
@@ -138,54 +138,38 @@ Write an efficient algorithm for the following assumptions:
 /* } */
 
 
+//100%
 int solution(int A[], int N) {
-        int i;
-        int B[100001] = {0};
-	int max       = A[0];
+	int i;
+	int result_a = A[0];
+	int result_i = 1;
 
-        if (N == 1) {
-                if (A[0] == 1)
-                        return 1;
-                else 
-                        return 0;
-        }
-
-        if (N == 2) {
-                if ( (fabs(A[0] - A[1]) == 1) && (A[0] == 1 || A[1] == 1) )
-                        return 1;
-                else 
-                        return 0;
-        }
-
-        for (i = 0; i < N; ++i) {
-		if (max < A[i])
-			max = A[i];
-
-		if (B[A[i]] == 0) 
-			B[A[i]]++;
-		else 
-			return 0;
+	for (i = 1; i < N; ++i) {
+		result_a ^= A[i];
+		result_i ^= i + 1;
 	}
 
-	if (max != N)
+	result_i = result_i ^ result_a;
+
+	if (result_i == 0)
+		return 1;
+	else 
 		return 0;
-
-	return 1;
-
-        /* for (i = 0; i < N + 1; ++i) { */
-        /*         if (B[i] != 1) */
-        /*                 return 0; */
-        /* } */
-        /* return 1; */
 }
 
 int main() 
 {
 	int i;
-	int N = 4;
+	int N = 9;
+	/* int A[] = {9, 3, 9, 3, 9, 7, 9}; */
+	/* int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; */
+	/* int A[] = {1, 3, 4, 2, 7, 5, 6, 9, 8}; */
+	int A[] = {1, 3, 2, 2, 9, 5, 6, 9, 8};
+	/* int A[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; */
+
 	/* int A[] = {1}; */
 	/* int A[] = {4, 3}; */
-	int A[] = {4, 1, 3, 2};
+	/* int A[] = {4, 1, 3, 2}; */
 
 
 	printf ("%d\n", solution(A, N));
